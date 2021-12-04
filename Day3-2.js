536ea5be6703c6b296ee668d7207a75f35e8e974
Ana Carolina K <anacko@gmail.com>
2021-12-03 19:25:36 -0500

    Refactor and fix to return correct value

--- Day3-2.js
+++ Day3-2.js
@@ -12,12 +12,8 @@ let example = `00100
 01010`
 const formatInput = input => input.split('\n')
 example = formatInput(example)
-console.log(example)
 
 const getLifeSupport = function(input) {
-
-  let oxyGenIdx = [...Array(input.length).keys()];
-  let CoScrubberIdx = [...Array(input.length).keys()];
 
   const checkPopularity = function(input, pos) {
     let idx0 = [];
@@ -25,47 +21,55 @@ const getLifeSupport = function(input) {
     input.map((elem, index) => elem[pos] === '0' ? idx0.push(index) : idx1.push(index))
     return { idx0, idx1 }
   }
-
-  let pos = 0;
-  while(oxyGenIdx.length > 1) {
-    console.log("input before all:", input)
-    let indexes = checkPopularity(input, pos)
-    console.log("Indexes: ", indexes, "pos: ", pos)
-
-    if (indexes.idx0.length > indexes.idx1.length) {
-      input = input.filter((value, index) => indexes.idx0.includes(index))
-      oxyGenIdx = oxyGenIdx.filter(elem => indexes.idx0.includes(elem))
-    
-    } else {
-      input = input.filter((value, index) => indexes.idx1.includes(index))
-      oxyGenIdx = oxyGenIdx.filter(elem => indexes.idx1.includes(elem))
-    }
-    console.log("oxyGenIdx: ", oxyGenIdx)
-    pos++;
+  
+  // Initial separation for 1st digit (position 0), common input
+  let indexes = checkPopularity(input, 0)
+  let oxyInput = [];
+  let coInput = [];
+  if (indexes.idx0.length > indexes.idx1.length) {
+    oxyInput = input.filter((value, index) => indexes.idx0.includes(index))
+    coInput = input.filter((value, index) => indexes.idx1.includes(index))
+  } else {
+    oxyInput = input.filter((value, index) => indexes.idx1.includes(index))
+    coInput = input.filter((value, index) => indexes.idx0.includes(index))
   }
-  console.log(input[oxyGenIdx[0]])
-
-  pos = 0;
-  while(CoScrubberIdx.length > 1 & pos < 15) {
-    let indexes = checkPopularity(CoScrubberIdx, pos)
+  
+  // For Oxygen Generator Rating: gets most popular, by digit position
+  let pos = 1;
+  while(oxyInput.length > 1) {
+    indexes = checkPopularity(oxyInput, pos);
     if (indexes.idx0.length > indexes.idx1.length) {
-      CoScrubberIdx = CoScrubberIdx.filter(elem => indexes.idx1.includes(elem))
+      oxyInput = oxyInput.filter((value, index) => indexes.idx0.includes(index));
     } else {
-      CoScrubberIdx = CoScrubberIdx.filter(elem => indexes.idx0.includes(elem))
+      oxyInput = oxyInput.filter((value, index) => indexes.idx1.includes(index));
     }
     pos++;
   }
 
-    return [oxyGenIdx, CoScrubberIdx]
-    //return Number(input[oxyGenIdx[0]]) * Number(input[CoScrubberIdx[0]])
+  // For CO2 Scrubber Rating: gets least popular, by digit position
+  pos = 1;
+  while(coInput.length > 1 & pos < 15) {
+    indexes = checkPopularity(coInput, pos);
+    if (indexes.idx0.length > indexes.idx1.length) {
+      coInput = coInput.filter((value, index) => indexes.idx1.includes(index))
+    } else {
+      coInput = coInput.filter((value, index) => indexes.idx0.includes(index))
+    }
+    pos++;
+  }
+
+  // Convert to decimal and multiply
+  return parseInt(oxyInput[0], 2) * parseInt(coInput[0], 2)
 }
 
 console.log(getLifeSupport(example))
+console.assert(typeof getLifeSupport(example) === 'number', 'Function not returning number.');
+console.assert(getLifeSupport(example) === 230, 'Function not returning correct value.');
 
-// Challenge 1
+// Challenge 2
 
-// const fs = require('fs');
-// fs.readFile('./Day3.txt', 'utf8', (err, data) => {
-//   let input = formatInput(data)
-//   console.log('Answer 3-1:', getLifeSupport(input))
-// })
+const fs = require('fs');
+fs.readFile('./Day3.txt', 'utf8', (err, data) => {
+  let input = formatInput(data)
+  console.log('Answer 3-2:', getLifeSupport(input))
+})
