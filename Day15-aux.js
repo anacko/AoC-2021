881f02d292a79b46c9d56e8b55cd5b20f1449eeb
Ana Carolina K <anacko@gmail.com>
2022-01-07 17:40:24 -0500

    update day 15 aux. still req. adjustments

--- Day15-aux.js
+++ Day15-aux.js
@@ -24,13 +24,13 @@
 3 4 5: jjjiii
 */
 
-for(i = 0; i < 6; i++) {
-  for(j = i+1; j < 6; j++) {
-    for(k = j+1; k < 6; k++) {
-      console.log(i, "-", j, "-", k)
-    }
-  }
-}
+// for(i = 0; i < 6; i++) {
+//   for(j = i+1; j < 6; j++) {
+//     for(k = j+1; k < 6; k++) {
+//       console.log(i, "-", j, "-", k)
+//     }
+//   }
+// }
 
 
 
@@ -39,13 +39,13 @@ const genConfigs = function(n, t) {
   for(i = 0; i < n; i++) { allConfig[0].push(i) }
 
   // move element in pos
-  let currentPos = 2 // n - 1 is last elem pos
+  let currentPos = n - 1 // last elem pos
   while(currentPos >=0){
 
     const currentConfig = [...allConfig[allConfig.length - 1]]
     let currentVal = currentConfig[currentPos]
     
-    for(i = 1; i < t - currentPos; i++) {
+    for(i = 1; i < t - n + currentPos; i++) {
       let newConfig = currentConfig.slice(0, currentPos)
       newConfig.push(currentVal + i)
       newConfig.push(...currentConfig.slice(currentPos + 1))
@@ -57,4 +57,37 @@ const genConfigs = function(n, t) {
 
   return allConfig
 }
-console.log(genConfigs(3, 6))
+//console.log(genConfigs(3, 6))
+
+
+let n = 5;
+let t = 7;
+const firstConfig = []
+for(i = 0; i < n; i++) { firstConfig.push(i) }
+
+const allConfig = [[...firstConfig]]
+
+const lastConfig = []
+for(i = t - n; i < t; i++) { lastConfig.push(i) }
+
+let k = 1
+while (k) {
+  const currentConfig = [...allConfig[allConfig.length - 1]]
+
+  k = 0;
+  while(k < lastConfig.length) {
+    if(currentConfig[k] !== lastConfig[k]) { k++; } else { break; }
+  }
+  let currentPos = k - 1;
+
+  let currentVal = currentConfig[currentPos]
+  while (currentVal < t - n + currentPos) {
+    currentVal++;
+    let newConfig = currentConfig.slice(0, currentPos)
+    newConfig.push(currentVal)
+    newConfig.push(...currentConfig.slice(currentPos + 1))  
+    allConfig.push(newConfig)
+  }
+}
+
+console.log(allConfig)
